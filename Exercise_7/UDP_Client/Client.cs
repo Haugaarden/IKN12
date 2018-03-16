e8ad566b3c9590aa978ccd90437761887122d225
Haugaarden <martin_haugaard8@hotmail.com>
2018-03-16 16:51:35 +0100

    tilføjede UDP projekt. Denne kan sende en besked

---
+++ Exercise_7/UDP_Client/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP
+{
+	class udp_client
+	{
+		private const int PORT = 9000;
+		private const int Buffer = 1000;
+
+		private udp_client(string[] args)
+		{
+
+			Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			var broadcast = IPAddress.Parse(args[0]);
+
+			byte[] sendbuf = Encoding.ASCII.GetBytes(args[1]);
+			var ep = new IPEndPoint(broadcast, PORT);
+
+			s.SendTo(sendbuf, ep);
+
+			Console.WriteLine("Sent");
+		}
+
+		private void sendData(String fileName, long fileSize, NetworkStream io)
+		{
+
+		}
+
+		public static void Main(string[] args)
+		{
+			new udp_client(args);
+		}
+	}
+}
---
+++ Exercise_7/UDP_Server/Server.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace UDP
+{
+	class udp_server
+	{
+		private const int PORT = 9000;
+		private const int Buffer = 1000;
+
+		private udp_server()
+		{
+
+			while(true)
+			{
+				var listener = new UdpClient(PORT);
+				var RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, PORT);
+
+				Byte[] receiveBytes = listener.Receive(ref RemoteIpEndPoint);
+
+				string returnData;
+				do
+				{
+					returnData = Encoding.ASCII.GetString(receiveBytes).ToLower;
+					Console.WriteLine(returnData);
+
+				} while(!returnData.Equals("u") || !returnData.Equals("l"));
+
+				string fileName;
+
+				if(returnData.Equals("u"))
+				{
+					fileName = "/proc/uptime";
+				} else
+				{
+					fileName = "/proc/loadavg";
+				}
+
+				Stream fileStream = File.OpenRead(fileName);	//Opens filestream
+
+				var fileBuffer = new byte[Buffer];	//Buffer to contain parts of the file
+
+				fileStream.Read(fileBuffer, 0, fileBuffer.Length);
+
+
+			}
+
+		}
+
+		private void sendData(String fileName, long fileSize, NetworkStream io)
+		{
+			
+		}
+
+		public static void Main(string[] args)
+		{
+			new udp_server();
+		}
+	}
+}
