5388fc03119b917776fd92f6cc1cc8409944ed43
Haugaarden <martin_haugaard8@hotmail.com>
2018-03-22 09:15:03 +0100

    Fiksede fejl i server. Bruger readtext i stedet for filestream. Client kan modtage fra server

--- Exercise_7/UDP_Client/Client.cs
+++ Exercise_7/UDP_Client/Client.cs
@@ -14,6 +14,7 @@ namespace UDP
 		{
 
 			Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			//var s = new UdpClient(PORT);
 			var broadcast = IPAddress.Parse(args[0]);
 
 			byte[] sendbuf = Encoding.ASCII.GetBytes(args[1]);
@@ -22,6 +23,11 @@ namespace UDP
 			s.SendTo(sendbuf, ep);
 
 			Console.WriteLine("Sent");
+
+			Byte[] receiveBytes = new byte[Buffer];
+			s.Receive(receiveBytes);
+			string returnData = Encoding.ASCII.GetString(receiveBytes).ToLower();
+			Console.WriteLine(returnData);
 		}
 
 		private void sendData(String fileName, long fileSize, NetworkStream io)
--- Exercise_7/UDP_Server/Server.cs
+++ Exercise_7/UDP_Server/Server.cs
@@ -14,45 +14,63 @@ namespace UDP
 		private udp_server()
 		{
 
-			while(true)
+			var listener = new UdpClient(PORT);
+			var RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, PORT);
+
+			//Byte[] receiveBytes = listener.Receive(ref RemoteIpEndPoint);
+
+			string returnData;
+			do
 			{
-				var listener = new UdpClient(PORT);
-				var RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, PORT);
-
 				Byte[] receiveBytes = listener.Receive(ref RemoteIpEndPoint);
+				returnData = Encoding.ASCII.GetString(receiveBytes).ToLower();
+				Console.WriteLine(returnData);
 
-				string returnData;
-				do
-				{
-					returnData = Encoding.ASCII.GetString(receiveBytes).ToLower();
-					Console.WriteLine(returnData);
+				Console.WriteLine(!returnData.Equals("u"));
 
-				} while(!returnData.Equals("u") || !returnData.Equals("l"));
-
-				string fileName;
-
-				if(returnData.Equals("u"))
-				{
-					fileName = "/proc/uptime";
-				} else
-				{
-					fileName = "/proc/loadavg";
-				}
-
-				Stream fileStream = File.OpenRead(fileName);	//Opens filestream
-
-				var fileBuffer = new byte[Buffer];	//Buffer to contain parts of the file
-
-				fileStream.Read(fileBuffer, 0, fileBuffer.Length);
+			} while(!returnData.Equals("u") && !returnData.Equals("l"));
 
 
+			string fileName;
+
+			if(returnData.Equals("u"))
+			{
+				fileName = "/proc/uptime";
+			} else
+			{
+				fileName = "/proc/loadavg";
 			}
+
+			Console.WriteLine(fileName);
+
+
+			string stringToSend = readFile(fileName);
+			sendString(stringToSend, listener, RemoteIpEndPoint);
 
 		}
 
-		private void sendData(String fileName, long fileSize, NetworkStream io)
+		private string readFile(string fileName)
 		{
-			
+			FileStream fs = File.OpenRead(fileName);	//Opens filestream
+
+			string fileText = System.IO.File.ReadAllText(fileName);
+
+			int fileSize = fileText.Length;
+
+			//var fileBuffer = new byte[Buffer];	//Buffer to contain parts of the file
+
+			//int fileSize = fs.Read(fileBuffer, 0, fileBuffer.Length);
+
+			Console.WriteLine(fileSize);
+
+			return fileText;
+		}
+
+		private void sendString(string stringToSend, UdpClient client, IPEndPoint remoteEP)
+		{
+			var bytesToSend = Encoding.ASCII.GetBytes(stringToSend);
+			client.Send(bytesToSend, bytesToSend.Length, remoteEP);
+			Console.WriteLine("Sent string");
 		}
 
 		public static void Main(string[] args)
