a5664121d53c155f59fb68a58f9fcd7a8b4dea8c
Haugaarden <martin_haugaard8@hotmail.com>
2018-05-17 10:31:27 +0200

    Lavede projekter til at teste link laget

---
+++ Exercise_11/ServerLinkOnly/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Library;
+
+using Linklaget;
+
+namespace ServerLinkOnly
+{
+	class MainClass
+	{
+		/// <summary>
+		/// The link.
+		/// </summary>
+
+		/// <summary>
+		/// The BUFSIZE
+		/// </summary>
+		private const int BUFSIZE = 1000;
+		private const string APP = "FILE_SERVER";
+
+		public static void Main(string[] args)
+		{
+			Link link;
+			link = new Link(BUFSIZE, APP);
+
+			Console.WriteLine("Hello World!");
+
+			string output = "AXBY Sut mig";
+
+			link.send(Encoding.ASCII.GetBytes(output), output.Length);
+
+			Console.WriteLine("Sent");
+		}
+	}
+}
